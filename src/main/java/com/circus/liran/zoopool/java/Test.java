675f996484c76eac0386b1675279f2ca3f31f2c9
lixuze <lixuze@iyuewe.com>
2013-09-24 11:34:39 +0800

    添加 枚舉練習

---
+++ src/main/java/com/circus/liran/zoopool/java/Test.java
@@ -0,0 +1,10 @@
+package com.circus.liran.zoopool.java;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(TestEnum.F);
+		System.out.println(TestEnum.F.name());
+		System.out.println(TestEnum.F.getValue());
+	}
+	
+}
---
+++ src/main/java/com/circus/liran/zoopool/java/TestEnum.java
@@ -0,0 +1,21 @@
+package com.circus.liran.zoopool.java;
+
+public enum TestEnum {
+
+	F(2),G(3),H(3);
+	private final int value;
+	
+	private TestEnum(int value)
+	{
+		this.value = value;
+	}
+	
+	public int getValue()
+	{
+		return this.value;
+	}
+	
+	
+
+	
+}
