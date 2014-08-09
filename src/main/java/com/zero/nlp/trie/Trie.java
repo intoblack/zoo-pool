3739cec405198d1006feea746f84ad2856e1d582
lixuze <intoblack86@gmail.com>
2014-08-09 20:20:13 +0800

    1.添加初步词典树结构

--- src/main/java/com/zero/nlp/trie/Trie.java
+++ src/main/java/com/zero/nlp/trie/Trie.java
@@ -1,16 +1,43 @@
 package com.zero.nlp.trie;
 
+import com.zero.nlp.utils.StringUtils;
 
-public class Trie {
-	private TrieNode rootNode = new TrieNode();
-	
-	public boolean insertWord(String word)
-	{
-		TrieNode currentNode = rootNode;
-		for(int i = 0 ; i < word.length() ; i++)
-		{
-			
+public class Trie implements ITrie<String> {
+	private TrieNode<Character> rootNode = new TrieNode<Character>();
+
+	public static void main(String[] args) {
+		Trie trie = new Trie();
+		trie.insert("abccdad");
+		trie.insert("adfdfss");
+		System.out.println(trie.search("abccdad"));
+		System.out.println(trie.search("abbb"));
+	}
+
+	@Override
+	public void insert(String value) {
+		if (StringUtils.isEmpty(value)) {
+			throw new IllegalArgumentException();
 		}
-		return false;
+		TrieNode<Character> currentNode = rootNode;
+		for (int i = 0; i < value.length(); i++) {
+			Character ch = value.charAt(i);
+			currentNode = currentNode.getTrieNode(ch);
+		}
+	}
+
+	@Override
+	public boolean search(String value) {
+		if (StringUtils.isEmpty(value)) {
+			throw new IllegalArgumentException();
+		}
+		TrieNode<Character> currentNode = rootNode;
+		for (int i = 0; i < value.length(); i++) {
+			Character ch = value.charAt(i);
+			currentNode = currentNode.contain(ch);
+			if (currentNode == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/main/java/com/zero/nlp/trie/TrieNode.java
+++ src/main/java/com/zero/nlp/trie/TrieNode.java
@@ -3,10 +3,25 @@ package com.zero.nlp.trie;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TrieNode {
+/**
+ * 
+ * 词典树节点信息保存　，　主要原理是每个词典树　，　包含节点变量　和数值和子节点 主要保存结构　map
+ * 
+ * @author lixuze
+ * 
+ * @param <T>
+ *            节点信息储存值　，　最好为int , char , double 等类型　
+ */
+public class TrieNode<T> {
 	private double value;
-	private Map<Character, TrieNode> wordMap = new HashMap<Character, TrieNode>();
+	private Map<T, TrieNode<T>> wordMap = new HashMap<T, TrieNode<T>>();
 
+	/**
+	 * 得到当前节点的信息值 最好用double
+	 * 
+	 * 
+	 * @return
+	 */
 	public double getValue() {
 		return value;
 	}
@@ -15,15 +30,56 @@ public class TrieNode {
 		this.value = value;
 	}
 
-	public void addChild(Character word, TrieNode node) {
+	public void addChild(T word, TrieNode<T> node) {
 		this.wordMap.put(word, node);
 	}
 
-	public Map<Character, TrieNode> getWordMap() {
-		return wordMap;
+	public Map<T, TrieNode<T>> getWordMap() {
+		return new HashMap<T, TrieNode<T>>(wordMap);
 	}
 
-	public void setWordMap(Map<Character, TrieNode> wordMap) {
+	public void setWordMap(Map<T, TrieNode<T>> wordMap) {
 		this.wordMap = wordMap;
+	}
+
+	public boolean containsChar(T ch) {
+		if (ch == null) {
+			return false;
+		}
+		if (wordMap.containsKey(ch)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 * 返回此此层节点中　，　某个特定值的子节点
+	 * 
+	 * @param ch
+	 *            　节点的序号
+	 * @return 指定值特定的序号的子节点
+	 */
+	public TrieNode<T> getTrieNode(T ch) {
+		if (ch != null) {
+			if (wordMap.containsKey(ch)) {
+				return wordMap.get(ch);
+			} else {
+				TrieNode<T> node = new TrieNode<T>();
+				this.addChild(ch, node);
+				return wordMap.get(ch);
+
+			}
+		}
+		throw new IllegalArgumentException("参数不能为null");
+	}
+
+	public TrieNode<T> contain(T ch) {
+		if (ch != null) {
+			if (wordMap.containsKey(ch)) {
+				return wordMap.get(ch);
+			}
+		}
+		return null;
 	}
 }
